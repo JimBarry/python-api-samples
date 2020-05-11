eabfe08b7a32206034da747e648d69708d4fca93
Jim Barry <jimbarry@gmail.com>
2020-05-10 18:44:15 -0700

    Update BASIC.py

--- BASIC.py
+++ BASIC.py
@@ -1,15 +1,14 @@
 from arcgis.gis import GIS
 from IPython.display import display
 
-ptl = input('enter portal: ')
+ptl = input('enter portal name: ')
 unm = input('enter username: ')
 pwd = input('enter password: ')
 
 gis = GIS("https://" + ptl + ".maps.arcgis.com", username=unm, password=pwd)
 
-my_content = gis.content.search("jimbarry", item_type="Feature Layer")
+my_content = gis.content.search("nyc", item_type="Feature Layer")
 my_content
 
 for item in my_content:
     display(item)
-
