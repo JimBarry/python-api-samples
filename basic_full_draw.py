24082be1b24424200d3d76b6d8176437597943cf
Jim Barry <jimbarry@gmail.com>
2020-07-20 08:52:57 -0700

    Update basic_full_draw.py

--- basic_full_draw.py
+++ basic_full_draw.py
@@ -2,36 +2,50 @@ from arcgis.gis import GIS
 from IPython.display import display
 from getpass import getpass
 
-# split cell here
+# ---------------------
+
+# get user inputs
 ptl = input('enter arcgis online org name: ')
 unm = input('enter username: ')
 pwd = getpass('enter password: ')
 srt = input('enter feature layer search term: ')
 
-# split cell here
+# ---------------------
+
+# connect to org
 gis = GIS("https://" + ptl + ".maps.arcgis.com", username=unm, password=pwd)
 
-# split cell here
+# ---------------------
+
+# search for feature layers, display basic results
 my_content = gis.content.search(srt, item_type="Feature Layer")
 my_content
 
-# split cell here
+# ---------------------
+
+# display detailed results
 for item in my_content:
     display(item)
 
-# split cell here
+# ---------------------
+
+# display details of selected item
 idx = input('enter index of layer you want to draw, or [enter] for 0: ') or 0
 selected_item = my_content[idx]
 display(selected_item)
 
-# split cell here
+# ---------------------
+
+# load an empty map, set extent to that of the selected layer
 map1 = gis.map()
 map1.extent = selected_item.extent
 map1
 
-# split cell here
+# ---------------------
+
+# draw feature layer into the map
 map1.add_layer(selected_item)
-# keep in mind, sometimes takes 30-60 sec or so to draw, be patient
+# *** keep in mind, this sometimes takes 30-60 sec or so to draw, be patient
 
 # end
 
