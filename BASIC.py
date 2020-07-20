0b6407a9d3580b8f6d105dd474359563e9d8fc73
Jim Barry <jimbarry@gmail.com>
2020-07-20 08:50:25 -0700

    Update BASIC.py

--- BASIC.py
+++ BASIC.py
@@ -2,14 +2,31 @@ from arcgis.gis import GIS
 from IPython.display import display
 from getpass import getpass
 
-ptl = input('enter portal name: ')
+# ---------------------
+
+#get inputs from user
+ptl = input('enter arcgis online org name: ')
 unm = input('enter username: ')
 pwd = getpass('enter password: ')
+srt = input('enter feature layer search term: ')
 
+# ---------------------
+
+# connect to org
 gis = GIS("https://" + ptl + ".maps.arcgis.com", username=unm, password=pwd)
 
-my_content = gis.content.search("nyc", item_type="Feature Layer")
+# ---------------------
+
+# search items by keyword, show basic results
+my_content = gis.content.search(srt, item_type="Feature Layer")
 my_content
 
+# ---------------------
+
+# show full results
 for item in my_content:
     display(item)
+
+
+
+
