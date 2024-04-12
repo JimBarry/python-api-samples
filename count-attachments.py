d3cbbbd7f60bda357dd57bef12a4acb34eec556f
Jim Barry <jimbarry@gmail.com>
2024-04-12 16:49:48 -0700

    Update count-attachments.py

--- count-attachments.py
+++ count-attachments.py
@@ -1,17 +1,17 @@
 # -----
 
 from arcgis.gis import GIS
-portalId_HostedFeatureService = '904467ee2fbb45f1ac816dd5ff3768ed'
+portalId_HostedFeatureServiceItem = '904467ee2fbb45f1ac816dd5ff3768ed'
 gis = GIS('home')
 print(gis)
 
 # -----
 
-hfl_item = gis.content.get(portalId_HostedFeatureService)
+hfl_item = gis.content.get(portalId_HostedFeatureServiceItem)
 hfl_layer = hfl_item.layers[0] #just the first feature layer in the feature service
-hfl_fset = hfl_layer.query()
-hfl_features = hfl_fset.features
-attachments = hfl_layer.attachments
+hfl_featureset = hfl_layer.query()
+hfl_features = hfl_featureset.features
+hfl_attachments = hfl_layer.attachments
 print(hfl_item.url)
 
 # -----
@@ -22,7 +22,7 @@ for feat in hfl_features:
     countFeatures+=1
     attribs = feat.attributes
     oid = attribs.get('OBJECTID')
-    attach = attachments.get_list(oid)
+    attach = hfl_attachments.get_list(oid)
     numAttachments = len(attach)
     countAttachments = countAttachments + numAttachments
     strToPrint = 'record: ' + str(countFeatures) + ', OID: ' + str(oid) + \
